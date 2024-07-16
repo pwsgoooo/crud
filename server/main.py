b45de8cdcd2aed749919200d69591729fa7d3ca8
pwsgoooo <pws_01@naver.com>
2024-07-17 08:24:57 +0900

    moving client

--- server/main.py
+++ server/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI,Depends
 from fastapi.responses import HTMLResponse,RedirectResponse
 from sqlalchemy.orm import Session
 from typing import Any,List
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 import json
 from db.models.conn import CURSOR,SESSON,create_tables #,start_ctable
 from db.models.members import Members
@@ -21,21 +23,25 @@ async def startup_event():
     # start_ctable()
     create_tables()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get('/',response_class=HTMLResponse)
-async def get_root() -> HTMLResponse:
-    return RedirectResponse(url="/html_page") # return RedirectResponse(url='../client/home.html')
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "name": "World"})
 
 
 
 
-@app.get('/board') #,response_model=list[Members]
-async def get_board(db:Session = Depends(get_db))->List:
-    CURSOR.execute('select * from board.members')
-    lists = CURSOR.fetchall()
-    # lists = db.query(Members).all()
-    if len(lists) >= 0:
-        print("table not exist content ")
+@app.get("/write_board",response_class=HTMLResponse)
+async def write_board(request: Request):
+    return templates.TemplateResponse("write_board.html",{"request":request})
+
+# @app.get('/board') #,response_model=list[Members]
+# async def get_board(db:Session = Depends(get_db))->List:
+#     CURSOR.execute('select * from board.members')
+#     lists = CURSOR.fetchall()
+#     if len(lists) >= 0:
+#         print("table not exist content ")
 
 # @app.get('/board/{id}',response_model=list[Members])
 # async def get_board(id:str, db:Session = Depends(get_db)):
