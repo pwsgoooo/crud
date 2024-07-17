a1fccf759c5afb4ee86eadeca6331dfa5117d2db
pwsgoooo <pws_01@naver.com>
2024-07-17 16:06:30 +0900

    crudfinish

--- server/main.py
+++ server/main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,Form,HTTPException,Request
 from fastapi.responses import HTMLResponse,RedirectResponse
 from sqlalchemy.orm import Session
 from typing import Any,List
 from fastapi.templating import Jinja2Templates
-from fastapi import Request
-import json
+from pydantic import BaseModel
 from db.models.conn import CURSOR,SESSON,create_tables #,start_ctable
 from db.models.members import Members
 
@@ -30,20 +29,65 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "name": "World"})
 
 
+@app.get("/board",response_class=HTMLResponse)
+async def get_board(request: Request, db:Session=Depends(get_db)):
+    boards = db.query(Members).all()
+    return templates.TemplateResponse("board.html",{"request":request, "boards":boards})
+
+class AddMembers(BaseModel):
+    id : str
+    name: str
+    content: str
+
+@app.post("/board",response_class=HTMLResponse)
+async def write_board(request: Request, id:str = Form(...),name:str=Form(...),content:str=Form(...),db:Session=Depends(get_db)):
+    new_data = AddMembers(id=id, name=name, content=content)
+    new_row = Members(id=new_data.id, name= new_data.name, content=new_data.content)
+    db.add(new_row)
+    db.commit()
+    db.refresh(new_row)    
+    boards = db.query(Members).all()
+    return templates.TemplateResponse("board.html",{"request":request, "boards":boards})
 
 
-@app.get("/write_board",response_class=HTMLResponse)
-async def write_board(request: Request):
-    return templates.TemplateResponse("write_board.html",{"request":request})
+@app.get("/board/edit/{row_id}")
+async def edit_data_view(row_id:str, request: Request, db:Session=Depends(get_db)):
+    edit_row = db.query(Members).filter(Members.id==row_id).first()
+    boards = db.query(Members).all()
+    if edit_row:
+        return templates.TemplateResponse("board_edit.html",{"request":request, "edit_row":edit_row,"boards":boards})
+    else:
+        raise HTTPException(status_code=404,detail="editing row enable")
 
-# @app.get('/board') #,response_model=list[Members]
-# async def get_board(db:Session = Depends(get_db))->List:
-#     CURSOR.execute('select * from board.members')
-#     lists = CURSOR.fetchall()
-#     if len(lists) >= 0:
-#         print("table not exist content ")
 
-# @app.get('/board/{id}',response_model=list[Members])
-# async def get_board(id:str, db:Session = Depends(get_db)):
-#     listonlyuser = db.query(Members).filter(db.id == id)
-#     return listonlyuser
+class EditMember(BaseModel):
+    name: str
+    content: str
+
+@app.put("/board/edit/{row_id}")
+async def edit_complete_view(request: Request,row_id:str,edit:EditMember, db:Session=Depends(get_db)):
+    edit_row= db.query(Members).filter(Members.id==row_id).first()
+    if edit_row:
+        # db.delete(edit_row)
+        edit_row.name = edit.name
+        edit_row.content = edit.content
+        db.commit()
+        db.refresh(edit_row)
+
+        boards = db.query(Members).all()
+        return templates.TemplateResponse("board.html",{"request":request,"boards":boards})
+    else:
+        raise HTTPException(status_code=404,detail="not not")
+
+    
+
+@app.post("/board/delete/{row_id}")
+async def delete_data(row_id:str, db:Session=Depends(get_db)):
+    delete_row = db.query(Members).filter(Members.id==row_id).first()
+    if delete_row:
+        db.delete(delete_row)
+        db.commit()
+    else:
+        raise HTTPException(status_code=404,detail="not not")
+    
+    return HTMLResponse(status_code=302, headers={"Location":"/board"})
