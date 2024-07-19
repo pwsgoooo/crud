3464036c506e86dd815adac231a0034da719a26a
pwsgoooo <pws_01@naver.com>
2024-07-19 13:12:32 +0900

    plusStaticPath:css,assets

--- server/main.py
+++ server/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI,Depends,Form,HTTPException,Request
 from fastapi.responses import HTMLResponse,RedirectResponse
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from typing import Any,List
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 from pydantic import BaseModel
 from db.models.conn import CURSOR,SESSON,create_tables #,start_ctable
 from db.models.members import Members
+import pyautogui
 
 
 app = FastAPI(debug=True)
@@ -21,6 +25,12 @@ def get_db():
 async def startup_event():
     # start_ctable()
     create_tables()
+
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.absolute()/"static"),
+    name='static',
+)
 
 templates = Jinja2Templates(directory="templates")
 
@@ -41,13 +51,25 @@ class AddMembers(BaseModel):
 
 @app.post("/board",response_class=HTMLResponse)
 async def write_board(request: Request, id:str = Form(...),name:str=Form(...),content:str=Form(...),db:Session=Depends(get_db)):
-    new_data = AddMembers(id=id, name=name, content=content)
-    new_row = Members(id=new_data.id, name= new_data.name, content=new_data.content)
-    db.add(new_row)
-    db.commit()
-    db.refresh(new_row)    
     boards = db.query(Members).all()
-    return templates.TemplateResponse("board.html",{"request":request, "boards":boards})
+    for chekduple in boards:
+        if chekduple.id == id.strip():
+            # chk = pyautogui.alert('id가 기존 id들과 중복되지 않게 설정해야함 (•́ .̮ •̀)՞')
+            # print(chk)
+            return templates.TemplateResponse("board.html",{"request":request,"boards":boards,"msg":'id가 기존 id들과 중복되지 않게 설정해야함 (•́ .̮ •̀)՞'})
+    
+    try:
+        new_data = AddMembers(id=id.strip(), name=name, content=content)
+        new_row = Members(id=new_data.id, name= new_data.name, content=new_data.content)
+        db.add(new_row)
+        db.commit()
+    except IntegrityError as e:
+        db.rollback()
+        # Handle the duplicate key error (e.g., log the error, notify the user, retry, etc.)
+    finally:
+        db.refresh(new_row)    
+        boards = db.query(Members).all()
+        return templates.TemplateResponse("board.html",{"request":request, "boards":boards})
 
 
 @app.get("/board/edit/{row_id}")
